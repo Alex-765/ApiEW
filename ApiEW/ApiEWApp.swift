c2d4fad5ac21db2803dd12734f15ef99b504a64a
alexanderbater <bater.a@etoncollege.org.uk>
2023-03-17 16:38:24 +0000

    Initial Commit

---
+++ ApiEW/ApiEWApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiEWApp.swift
+//  ApiEW
+//
+//  Created by Alexander Bater on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ApiEWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApiEW/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ApiEW
+//
+//  Created by Alexander Bater on 17/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
